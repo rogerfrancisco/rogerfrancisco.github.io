c6129e5a45fd1a2cbbaf55ba9433163c33544914
rogerfrancisco <51387037+rogerfrancisco@users.noreply.github.com>
2019-06-06 20:16:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$("section").load('home.html');
+
+function home() {
+    $("section").load('home.html');
+}
+function perfil() {
+    $("section").load('perfil.html');
+}
+function projetos() {
+    $("section").load('projetos.html');
+}
+function contato() {
+    $("section").load('contato.html');
+}
